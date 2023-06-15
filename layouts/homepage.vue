ef0642429dc3125f2182552c1e61dc604c0d89f1
Quentin LEBOT <qlebot@4cad.fr>
2023-06-15 16:56:22 +0200

    fix link to splotter spellen

--- layouts/homepage.vue
+++ layouts/homepage.vue
@@ -1,7 +1,7 @@
 <template>
     <slot />
     <footer class="footer footer-center bg-base-300 absolute bottom-0">
-        <a src="https://www.splottershop.com/" target="_blank" class="inline-block" href="">Food chain magnate & Food chain
+        <a href="https://www.splottershop.com/" target="_blank" class="inline-block">Food chain magnate & Food chain
             magnate:
             The
             ketchup mechanism are games published
