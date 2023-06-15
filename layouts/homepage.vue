efcf2147e5a942ac7416c3c5b1b2afa9bef3d21f
Quentin LEBOT <qlebot@4cad.fr>
2023-06-15 16:51:15 +0200

    Add mentions

--- layouts/homepage.vue
+++ layouts/homepage.vue
@@ -1,3 +1,12 @@
 <template>
     <slot />
+    <footer class="footer footer-center bg-base-300 absolute bottom-0">
+        <a src="https://www.splottershop.com/" target="_blank" class="inline-block" href="">Food chain magnate & Food chain
+            magnate:
+            The
+            ketchup mechanism are games published
+            by
+            <span class="">Splotter Spellen &reg;</span>
+        </a>
+    </footer>
 </template>
